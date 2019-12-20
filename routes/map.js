88337aba6eaf70722e7e84df8f63370207087a2e
Sgearjhz <624881768@qq.com>
2019-12-20 15:42:37 +0800

    bug fixed

--- routes/map.js
+++ routes/map.js
@@ -6,8 +6,6 @@ module.exports = function init (request, globalValues) {
   wsConnection = globalValues.wsConnection;
 
   wsConnection.init(request, 'map', 'utf8', function (message) {
-    wsConnection.maps[request.resourceURL.query.user_id].selectedTag = request.resourceURL.query.tag_id; // save selectedTag from webpage
-    log.info('selectedTag saved.');
     try {
       messageHandler(message, wsConnection.maps[request.resourceURL.query.user_id]);
     } catch (e) {
